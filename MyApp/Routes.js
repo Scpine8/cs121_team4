b5b63a27ad35f274ee8b451deb5908b08b2bd734
scpine8 <scpine8@gmail.com>
2019-11-19 13:54:16 -0800

    added some comments nbd

--- MyApp/Routes.js
+++ MyApp/Routes.js
@@ -4,19 +4,15 @@ import HomeScreen from "./screens/HomeScreen";
 import ActionScreen from "./screens/Action";
 import React, { Component } from 'react';
 
+// Way to Control Navigating Between Screens:
+
 const AppContainer = createStackNavigator(
   {
-  // HomeScreen: {
-  //   screen: HomeScreen
-  // },
-  // Action: {
-  //   screen: ActionScreen
-  // },
-  HomeScreen: {screen: HomeScreen},
-  Action: {screen: ActionScreen},
+  HomeScreen: {screen: HomeScreen}, // Set the HomeScreen to identifier 'HomeScreen'
+  Action: {screen: ActionScreen}, // Set the ActionScreen to identifier 'Action'
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'HomeScreen', // Set default screen to 'HomeScreen'
   }
 );
-export default createAppContainer(AppContainer);
+export default createAppContainer(AppContainer); // Export navigator in an app container to be accessed by other files
